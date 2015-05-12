f0864e6b225d3071794e831adf5aebabc7df8320
stengun <robenfi@gmail.com>
2015-05-13 00:33:28 +0200

    Modified database helper skeleton, added a simple "game set" db controller.

---
+++ src/data/DatabaseHelper.cpp
@@ -0,0 +1,84 @@
+/* 
+ * File:   DatabaseHelper.cpp
+ * Author: Roberto Benfatto
+ * 
+ * Created on 6 maggio 2015, 14.29
+ */
+
+#include "DatabaseHelper.h"
+#include <QtSql/QSqlError>
+
+DatabaseHelper::DatabaseHelper(const QString dbpath, unsigned int version, const QString uniquename) {
+    if (version < 1) throw "Error: database version must be > 0.";
+    this->version = version;
+    this->initializing = false;
+    this->name = dbpath;
+    this->db = QSqlDatabase()::addDatabase("QSQLITE", uniquename);
+    this->db.setDatabaseName(dbpath);
+    if (this->db.open()) {
+        onOpen(db);
+        onConfig(db);
+        int ver = getVersion();
+        if (ver == 0) onCreate(db);
+        else if (ver < version) onUpdate(db, ver, version);
+        else if (ver > version) onDowngrade(db, ver, version);
+        setVersion(version);
+    } else {
+        throw "Error: cannot open database";
+    }
+}
+
+QSqlQuery DatabaseHelper::query(const QString str_query, std::list<auto> params) {
+    if (db.isOpen()) {
+        db.transaction();
+        QSqlQuery qry = QSqlQuery(db);
+        qry.prepare(str_query);
+        if (!params.empty()) {
+            for (auto val : params) {
+                qry.addBindValue(val);
+            }
+        }
+        qry.exec();
+        qry.finish();
+        if (qry.lastError().type() != QSqlError::NoError) {
+            db.rollback();
+            //Insert logging things here
+        } else {
+            db.commit();
+        }
+        return qry;
+    } else {
+        throw new QSqlError("Can't query database while closed");
+    }
+}
+
+void DatabaseHelper::close() {
+    std::lock_guard<std::mutex> Guard(this->Mutex);
+    if (this->initializing) throw "Error: invalid state";
+    if (this->db.isOpen()) {
+        this->db.close();
+    }
+}
+
+int DatabaseHelper::getVersion() {
+    QSqlQuery qry = this->query("pragma user_version");
+    if (qry.lastError().type() == QSqlError::NoError) {
+        return qry.value(0).toInt();
+    }
+    throw "Error getting database version.";
+}
+
+void DatabaseHelper::setVersion(int version) {
+    QSqlQuery qry = this->query("pragma user_version=?", { version });
+    if (qry.lastError().type() == QSqlError::NoError) {
+        this->version = version;
+        return;
+    }
+    throw "Error setting database version.";
+}
+
+DatabaseHelper::~DatabaseHelper() {
+    close();
+    QSqlDatabase::removeDatabase(db.connectionName());
+}
+
--- src/data/setcontainers.h
+++ src/data/setcontainers.h
@@ -15,27 +15,29 @@
 class ItemCollection {
     public:
         //ItemCollection();
-        ItemCollection(QString* name, QString* parent=NULL);
-        void setName(QString* name) { this->name = name; };
-        QString* getName() { return this->name; };
-        void setParent(QString* parent) { this->parent = parent; };
-        QString* getParent() { return this->parent; };
+        ItemCollection(QString name, QString parent=nullptr);
+        void setName(QString name) { this->name = name; };
+        QString getName() const { return this->name; };
+        void setParent(QString parent) { this->parent = parent; };
+        QString getParent() const { return this->parent; };
         void setStatus(status_t status) { this->set_status = set_status; };
-        status_t getStatus() { return this->set_status; };
+        status_t getStatus() const { return this->set_status; };
         virtual set_t getType() const = 0;
         virtual ~ItemCollection();
+        QHash::const_iterator getItemIteratorBegin() { return itemlist.begin(); };
+        QHash::const_iterator getItemIteratorEnd() { return itemlist.end(); };
         bool addItem(SItem item);
         bool delItem(SItem item);
         bool delItem(QString item);
         bool hasItem(SItem item);
     private:
-        QString* name;
-        QString* parent;
-        QHash<QString, SItem> itemlist;
+        QString name;
+        QString parent;
+        QHash<QString, auto> itemlist;
         status_t set_status;
 };
 
-class Bios : public ItemCollection {
+class Bios : public Game {
     public:
         Bios(QString* name, QString* parent=NULL) : ItemCollection(name, parent) {};
         set_t getType() { return set_t.TYPE_BIOS; };
@@ -45,8 +47,31 @@ class Bios : public ItemCollection {
 class Game : public ItemCollection {
     public:
         Game(QString* name, QString* parent=NULL) : ItemCollection(name, parent) {};
-        set_t getType() { return set_t.TYPE_GAME; };
+        QString getDescription() const {
+            return description;
+        }
+        void setDescription(QString description) {
+            this->description = description;
+        }
+        QString getManufacturer() const {
+            return manufacturer;
+        }
+        void setManufacturer(QString manufacturer) {
+            this->manufacturer = manufacturer;
+        }
+        int getYear() const {
+            return year;
+        }
+        void setYear(int year) {
+            this->year = year;
+        }
+        virtual set_t getType() { return set_t.TYPE_GAME; };
         virtual ~Game();
+    private:
+        int year;
+        QString description;
+        QString manufacturer;
+    
 };
 
 class Mechanical : public ItemCollection {
