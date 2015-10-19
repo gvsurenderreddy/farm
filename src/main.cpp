cd5b02cda5089e736db567dafabb42d2e53afb67
Roberto Benfatto <robenfi@gmail.com>
2015-10-19 19:41:35 +0200

    Add simple test for ItemCollection.

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,21 @@ int main(int argc, char** argv)
     QFile *datfile = new QFile("/home/stengun/mame166.dat");
     dpars.startRead(datfile);
     int count = 0;
-    ItemCollection *col = dpars.readObject();
-    while(col != nullptr) {
+    while(count < 5) {
+        ItemCollection *col = dpars.readObject();
         std::cout << "Machine " << col->getName().toStdString() << std::endl;
         std::cout << "Description " << col->getDescription().toStdString() << std::endl;
         std::cout << "Manufacturer " << col->getManufacturer().toStdString() << std::endl;
         std::cout << "Year " << col->getYear() << std::endl;
         std::cout << "Items " << col->getItemQuantity() << std::endl;
+        QHash<QString, SItem>::iterator it = col->getItemIteratorBegin();
+        for(it; it != col->getItemIteratorEnd(); ++it){
+            std::cout << "rom " << it->getName().toStdString() << " "
+                    << it->getSha1().toStdString() << " "
+                    << it->getCrc().toStdString() << " " << it->getType() << std::endl;
+        }
+
         std::cout << "-------------------------------------" << std::endl;
-        col = dpars.readObject();
         count++;
     }
     std::cout << "PARSED ROMS " << count << std::endl;
